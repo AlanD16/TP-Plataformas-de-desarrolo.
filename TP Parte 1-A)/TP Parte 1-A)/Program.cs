6fb8193c990676271d03310ba4ff0fea3be0bf08
AlanD16 <50204705+AlanD16@users.noreply.github.com>
2019-06-30 15:02:36 -0300

    Add files via upload

---
+++ TP Parte 1-A)/TP Parte 1-A)/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Parte_1_A_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("ingresar nombre: ");
+            var nombre = Console.ReadLine();
+            Console.WriteLine("Ingresar apellido: ");
+            var apellido = Console.ReadLine();
+
+            Console.WriteLine(nombre + " " + apellido);
+            Console.ReadKey();
+        }
+    }
+}
